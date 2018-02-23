2bded62f4763457248229ea9c65c27a287aa3572
Pedro <Pedro@Pedro-PC.home>
2018-02-22 22:32:02 -0300

    aula01

---
+++ Service_Desk_81613477/src/br/usjt/ftce/arqsw/entity/Fila.java
@@ -0,0 +1,23 @@
+package br.usjt.ftce.arqsw.entity;
+
+public class Fila {
+
+	private int id;
+	private String Nome ;
+	private int x;
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	
+}
